e9321e83d4a158420ed490b470d7db2c29731451
PJ Singh <prabhjeets@yahoo.com>
2014-10-30 23:11:57 -0700

    Initial Commit

---
+++ Blocstagram/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Blocstagram
+//
+//  Created by Prabhjeet Singh on 10/30/14.
+//  Copyright (c) 2014 PJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
